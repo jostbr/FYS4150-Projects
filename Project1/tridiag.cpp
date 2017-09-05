50cdf36cf42d08570f44d51db1c11725d41b2b35
jostbr <jostein.brandshoi@gmail.com>
2017-09-05 19:02:27 +0200

    Finalized general and specilized tridiag algorithm.

--- Project1/tridiag.cpp
+++ Project1/tridiag.cpp
@@ -4,90 +4,148 @@
 # include <fstream>
 # include <cstdlib>
 # include <cmath>
+# include <ctime>
+# include <sstream>
 
-std::ofstream ofile;
-
-void tridiag_gen(double*, double*, double*, double*, int, double**);
+/* Functions defined further below in the program. */
+void tridiag_general(double*, double*, double*, double*, int, double*);
+void tridiag_specialized(double*, int, double*);
+double source_term(double);
 double exact(double);
+void write_results_to_file(const char*, double*, double*, double*, int);
 
 int main(int argc, char* argv[]){
-    const char* fileout = "output.txt";
-	int N = atoi(argv[1]);
-	double h = 1.0/((double)(N - 1));
-	double* x = new double[N];
+	int upper_exponent = atoi(argv[1]);
+    int N;
 
-	double* a = new double[N-3];	// Lower diagonal
-	double* b = new double[N-2];		// Main diagonal
-	double* c = new double[N-3];	// Upper diagonal
-	double* y = new double[N-2];		// Right side of diff. eq.
-	double* sol = new double[N-2];		// To hold solution
-	double* numerical = new double[N];		// To hold solution
-	double* analytical = new double[N];
+    for (int i = 1; i <= upper_exponent; i++){
+        N = pow(10, i);
 
-	for (int i = 0; i < N-2; i++){
-		if (i < N-3){
-			a[i] = -1.0;
-			c[i] = -1.0;
-		}
+        std::string fileout = "result";
+        std::ostringstream convert;         // Stream used for the conversion
+        convert << N;                       // Insert the text rep of N in the stream
+        std::string result = convert.str(); // Set to the contents of the stream
+        fileout.append(result);
+        fileout.append(".txt");
+        std::cout << fileout << std::endl;
+    
+    	double h = 1.0/((double)(N + 1));
+    	double* x = new double[N];
 
-		b[i] = 2.0;
-        y[i] = h*h*100.0*exp(-10*x[i]);
+    	double* a = new double[N-1];         // Lower diagonal
+    	double* b = new double[N];         // Main diagonal
+    	double* c = new double[N-1];         // Upper diagonal
+    	double* g = new double[N];         // Right side of diff. eq.
+    	double* interior = new double[N];  // To hold solutionution
+    	double* numerical = new double[N+2];   // To hold solutionution
+    	double* analytical = new double[N+2];  // To hold analytical solusolution
+        double h_squared = h*h;              // To save computing h*h every loop
+
+        for (int i = 0; i < N-1; i++) a[i] = -1.0;
+    	for (int i = 0; i < N-1; i++) c[i] = -1.0;
+        for (int i = 0; i < N; i++) b[i] = 2.0;
+
+        for (int i = 0; i < N+2; i++){
+    		x[i] = i*h;
+    		analytical[i] = exact(x[i]);
+    	}
+
+        for (int i = 0; i < N; i++){
+            g[i] = h_squared*source_term(x[i+1]);
+        }
+
+    	numerical[0] = numerical[N+1] = 0.0;              // Boundary conditions
+        clock_t start_time = clock();
+    	tridiag_general(a, b, c, g, N, interior);     // Solution for interior
+        //tridiag_specialized(g, N, interior);
+        clock_t end_time = clock();
+        double time_used = (double)(end_time - start_time)/CLOCKS_PER_SEC;
+        std::cout << std::setw(15) << std::setprecision(10) << "Time :" << time_used << "s" << std::endl;
+
+    	for (int i = 1; i < N+1; i++){
+    		numerical[i] = interior[i-1];
+    	}
+
+        write_results_to_file(fileout.c_str(), x, numerical, analytical, N+2);
     }
-
-    for (int i = 0; i < N; i++){
-		x[i] = i*h;
-		analytical[i] = exact(x[i]);
-	}
-
-	numerical[0] = numerical[N] = 0.0;
-
-	tridiag_gen(a, b, c, y, N-2, &sol);
-
-	int j = 0;
-
-	for (int i = 1; i <= N-1; i++){
-		numerical[i] = sol[j];
-		j += 1;
-	}
-
-	ofile.open(fileout);
-    ofile << std::setiosflags(std::ios::showpoint | std::ios::uppercase);
-    //      ofile << "       x:             approx:          exact:       relative error" << endl;
-    for (int i = 1; i < N-1; i++) {
-        double xval = x[i];
-        double RelativeError = fabs((exact(xval)-numerical[i])/exact(xval));
-        ofile << std::setw(15) << std::setprecision(8) << xval;
-        ofile << std::setw(15) << std::setprecision(8) << numerical[i];
-        ofile << std::setw(15) << std::setprecision(8) << exact(xval);
-        ofile << std::setw(15) << std::setprecision(8) << log10(RelativeError) << std::endl;
-    }
-
-    ofile.close();
 
 	return 0;
 }
 
-void tridiag_gen(double* a, double* b, double* c, double* y, int N, double** sol){
-	double* b_tilde = new double[N-1];
-	double* y_tilde = new double[N-1];
-
-	b_tilde[0] = b[1] - a[0]*a[0]/b[0];
-	y_tilde[0] = y[1] - (a[0]/b[0])*y[0];
-
-	for (int i = 1; i < N-1; i++){
-		b_tilde[i] = b[i] - a[i-1]*c[i-1]/b_tilde[i-1];
-		y_tilde[i] = y[i] - (a[i-1]/b_tilde[i-1])*y_tilde[i-1];
+void tridiag_general(double* a, double* b, double* c, double* y, int N, double* solution){
+	for (int i = 1; i < N; i++){
+		b[i] = b[i] - a[i-1]*a[i-1]/b[i-1];
+		y[i] = y[i] - (a[i-1]/b[i-1])*y[i-1];
 	}
 
-	(*sol)[N-1] = y_tilde[N-2]/b_tilde[N-2];
+	solution[N-1] = y[N-1]/b[N-1];
 
-	for (int i = N-2; i > 0; i--){
-		(*sol)[i] = (y[i] - c[i]*(*sol)[i+1])/b_tilde[i];
+	for (int i = N-2; i >= 0; i--){
+		solution[i] = (y[i] - c[i]*solution[i+1])/b[i];
 	}
-
-	(*sol)[0] = (y[0] - c[0]*(*sol)[2])/b[0];
 }
+
+void tridiag_specialized(double* y, int N, double* solution){
+    double* b = new double[N];
+    b[0] = 2;
+
+    for (int i = 1; i < N; i++){
+        b[i] = (i + 2)/((double)(i + 1));
+        y[i] = y[i] + (y[i-1]/b[i-1]);
+    }
+
+    solution[N-1] = y[N-1]/b[N-1];
+
+    for (int i = N-2; i >= 0; i--){
+        solution[i] = (y[i] + solution[i+1])/b[i];
+    }
+}
+
+
+double source_term(double x){
+    return 100.0*exp(-10.0*x);
+}
+
 
 double exact(double x){
     return 1 - (1 - exp(-10.0))*x - exp(-10.0*x);
 }
+
+
+void write_results_to_file(const char* filename, double* x, double* numerical, double* analytical, int N){
+    /* Write results to output file. */
+    std::ofstream ofile;
+    ofile.open(filename);
+    ofile << std::setiosflags(std::ios::showpoint | std::ios::uppercase);
+    ofile << "       x:             approx:          exact:       relative error" << std::endl;
+
+    for (int i = 0; i < N; i++) {
+        double x_val = x[i];
+        double rel_error = fabs((exact(x_val)-numerical[i])/exact(x_val));
+        ofile << std::setw(20) << std::setprecision(8) << x_val;
+        ofile << std::setw(20) << std::setprecision(8) << numerical[i];
+        ofile << std::setw(20) << std::setprecision(8) << exact(x_val);
+        ofile << std::setw(20) << std::setprecision(8) << log10(rel_error) << std::endl;
+    }
+
+    ofile.close();
+}
+
+/*void tridiag_general(double* a, double* b, double* c, double* y, int N, double* solution){
+    double* b = new double[N];
+    double* y = new double[N];
+
+    b[0] = b[0];
+    y[0] = y[0];
+
+    for (int i = 1; i < N; i++){
+        b[i] = b[i] - a[i-1]*a[i-1]/b[i-1];
+        y[i] = y[i] - (a[i-1]/b[i-1])*y[i-1];
+    }
+
+    solution[N-1] = y[N-1]/b[N-1];
+
+    for (int i = N-2; i >= 0; i--){
+        solution[i] = (y[i] - c[i]*solution[i+1])/b[i];
+    }
+}*/
--- Project1/visualize_results.py
+++ Project1/visualize_results.py
@@ -4,24 +4,39 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+upper_exponent = int(sys.argv[1])
+problem_sizes = np.logspace(1, upper_exponent, upper_exponent,
+    endpoint = True).astype(int)
+num_boundary_points = 2
+
 os.system("g++ -o tridiag tridiag.cpp")
-os.system("./tridiag {}".format(sys.argv[1]))
+os.system("./tridiag {}".format(upper_exponent))
 
-x = list()
-numerical = list()
-analytical = list()
-rel_error = list()
+plt.figure(figsize = (8, 7))
+plt.style.use("seaborn")
 
-with open("output.txt", "r") as results:
-    for line_num, line_string in enumerate(results):
-        words = line_string.split()
-        x.append(float(words[0]))
-        numerical.append(float(words[1]))
-        analytical.append(float(words[2]))
-        rel_error.append(float(words[3]))
+for size in problem_sizes:
+    full_system_size = size + num_boundary_points     # Need to account for boundary points as well
+    x = np.zeros(full_system_size)
+    numerical = np.zeros(full_system_size)
+    analytical = np.zeros(full_system_size)
+    #rel_error = np.zeros(size)
 
-plt.style.use("ggplot")
-plt.plot(x, numerical, linewidth = 2, label = "numerical")
+    with open("result{}.txt".format(size), "r") as results:
+        for line_num, line_string in enumerate(results):
+            if (line_num > 0):
+                words = line_string.split()
+                x[line_num-1] = float(words[0])
+                numerical[line_num-1] = float(words[1])
+                analytical[line_num-1] = float(words[2])
+                #rel_error[line_num-1] = float(words[3])
+
+        plt.plot(x, numerical, linewidth = 2, label = "numerical {}".format(size))
+
 plt.plot(x, analytical, linewidth = 2, label = "analyical")
+plt.xlabel("$x$-coordinate", fontname = "serif", fontsize = 16)
+plt.ylabel("$u(x)$", fontname = "serif", fontsize = 16)
+plt.title("Solution to 1D Poisson eq. (gen. alg.)",
+    fontname = "serif", fontsize = 18)
 plt.legend()
 plt.show()
