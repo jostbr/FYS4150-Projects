776163983cda5390721ebe7aedc03e45d9396976
tbhjelmeland <t.b.hjelmeland@fys.uio.no>
2017-09-20 10:31:07 +0200

    gives right eigenvalues, but do not terminate correctly

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,4 +1,4 @@
-//Jacobi Rotation Algorithm
+﻿//Jacobi Rotation Algorithm
 
 
 #include <iostream>
@@ -14,16 +14,17 @@ int main()
 {
     int n = 3;
     mat A(n,n);
-    A(0,0) = 2.0;
-    A(0,1) = 3.0;
-    A(0,2) = 5.0;
-    A(1,0) = 3.0;
-    A(1,1) = 6.0;
-    A(1,2) = 1.0;
-    A(2,0) = 5.0;
-    A(2,1) = 1.0;
-    A(2,2) = -4.0;
+    A(0,0) = 3.0;
+    A(0,1) = 1.0;
+    A(0,2) = -1.0;
+    A(1,0) = 1.0;
+    A(1,1) = 3.0;
+    A(1,2) = -1.0;
+    A(2,0) = -1.0;
+    A(2,1) = -1.0;
+    A(2,2) = 5.0;
     A.print("A= ");
+    //R should return 2,3,6 along the diagonal
 
     mat R = eye(n,n);
     R.print("R= ");
@@ -42,16 +43,18 @@ int main()
         }
     }
     cout << "Max value =" << max << endl;
+    cout << "k=" << k << endl;
+    cout << "l=" << l << endl;
 
     double epsilon = 1.0e-8;
-    double max_number_iterations =  n * n * n;
+    double max_number_iterations =  3*n;
     int iterations = 0;
-
+    double s, c;
+    double t, tau;
 
     while ( fabs(max) > epsilon && (double) iterations < max_number_iterations ){
-        double s, c;
+        cout << "Max value 1=" << max << endl;
         if ( A(k,l) != 0.0 ) {
-            double t, tau;
             tau = (A(l,l) - A(k,k))/(2*A(k,l));
             if ( tau > 0 ) {
                 t = 1.0/(tau + sqrt(1.0 + tau*tau));
@@ -67,11 +70,13 @@ int main()
         double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
         a_kk = A(k,k);
         a_ll = A(l,l);
+        A.print("A= ");
         // changing the matrix elements with indices k and l
         A(k,k) = c*c*a_kk - 2.0*c*s*A(k,l) + s*s*a_ll;
         A(l,l) = s*s*a_kk + 2.0*c*s*A(k,l) + c*c*a_ll;
         A(k,l) = 0.0; // hard-coding of the zeros
         A(l,k) = 0.0;
+        A.print("A= ");
         // and then we change the remaining elements
         for ( int i = 0; i < n; i++ ) {
             if ( i != k && i != l ) {
@@ -88,9 +93,36 @@ int main()
                 R(i,k) = c*r_ik - s*r_il;
                 R(i,l) = c*r_il + s*r_ik;
                 }
+        A.print("A= ");
+        R.print("R= ");
+        double max = 10e-9;
+        cout << "Max value 2=" << max << endl;
+        for (int i = 0; i < n; i++){
+            for (int j = i+1; j < n; j++){
+                if (fabs(A(i,j)) > max){
+                    max = fabs(A(i,j));
+                    k = i;
+                    l = j;
+                    cout << "k=" << k << endl;
+                    cout << "l=" << l << endl;
+                    cout << "Max value 3=" << max << endl;
+                    cout << "Number of iterations=" << iterations << endl;
+
+                }
+                else{
+                    cout << "Jacobi rotational algorithm converged" << endl;
+
+                }
+
+
+            }
+        }
         iterations++;
+
     }
     cout << "Number of iterations: " << iterations << endl;
+    R.print("R= ");
+    A.print("A= ");
 
 
     return 0;
