110a3ea420a7a79136d0c75897e3d21fc297ef6f
jostbr <jostein.brandshoi@gmail.com>
2017-09-26 22:36:27 +0200

    Updated arguments for ortho_transform() to fix confusion.

--- Project2/jacobi.cpp
+++ Project2/jacobi.cpp
@@ -29,8 +29,8 @@ double get_max_non_diag(arma::mat A, int N, int* k, int* l){
 void get_trig_values(arma::mat A, int k, int l, double* cosine, double* sine){
     if (A(k,l) != 0.0){         // More work needed if A(k,l) != 0
         double tau = (A(l,l) - A(k,k))/(2*A(k,l));              // Collecting variables
-        double tangent_1 = -tau + sqrt(1 + pow(tau, 2.0));      // Largest root
-        double tangent_2 = -tau - sqrt(1 + pow(tau, 2.0));      // Smallest root
+        double tangent_1 = -tau + sqrt(1 + tau*tau);      // Largest root
+        double tangent_2 = -tau - sqrt(1 + tau*tau);      // Smallest root
 
         *cosine = 1/(sqrt(1 + pow(tangent_2, 2.0)));    // Trig identity for cosine
         *sine = tangent_2*(*cosine);                    // Trig identity for sine
@@ -42,27 +42,32 @@ void get_trig_values(arma::mat A, int k, int l, double* cosine, double* sine){
     }
 }
 
-void ortho_transform(arma::mat* A, int N, int k, int l, double sine, double cosine){
-    double a_kk = (*A)(k,k);
-    double a_ll = (*A)(l,l);
+void ortho_transform(arma::mat* A, int N, int k, int l, double cosine, double sine){
+    double a_kk = (*A)(k,k);    // To avoid using overwritten value below
+    double a_ll = (*A)(l,l);    // To avoid using overwritten value below
     double a_ik, a_il;
 
+    /* Loop through diagonal of matrix. */
     for (int i = 0; i < N; i++){
+        /* Update values next to diagonal. */
         if ((i != k) && (i != l)){
-            a_ik = (*A)(i,k);
-            a_il = (*A)(i,l);
+            a_ik = (*A)(i,k);       // To avoid using overwritten value below
+            a_il = (*A)(i,l);       // To avoid using overwritten value below
             (*A)(i,i) = (*A)(i,i);
             (*A)(i,k) = a_ik*cosine - a_il*sine;
-            (*A)(k,i) = (*A)(i,k);
+            (*A)(k,i) = (*A)(i,k);      // Dealing with a symmetric matrix
             (*A)(i,l) = a_il*cosine + a_ik*sine;
-            (*A)(l,i) = (*A)(i,l);
+            (*A)(l,i) = (*A)(i,l);      // Dealing with a symmetric matrix
         }
     }
 
-    (*A)(k,k) = a_kk*pow(cosine, 2.0) - 2*(*A)(k,l)*cosine*sine + a_ll*pow(sine, 2.0);
-    (*A)(l,l) = a_ll*pow(cosine, 2.0) + 2*(*A)(k,l)*cosine*sine + a_kk*pow(sine, 2.0);
-    (*A)(k,l) = 0.0;
-    (*A)(l,k) = 0.0;
+    /* Update the two relevant values on the diagonal. */
+    (*A)(k,k) = a_kk*cosine*cosine - 2*(*A)(k,l)*cosine*sine
+            + a_ll*sine*sine;
+    (*A)(l,l) = a_ll*cosine*cosine + 2*(*A)(k,l)*cosine*sine
+            + a_kk*sine*sine;
+    (*A)(k,l) = 0.0;    // Defined cosine, sine require this to be zero
+    (*A)(l,k) = 0.0;    // Defined cosine, sine require this to be zero
 }
 
 void jacobi_eigen(arma::mat A, int N){
@@ -70,20 +75,20 @@ void jacobi_eigen(arma::mat A, int N){
 
     int k, l;
     double sine, cosine;
-    double curr_max_nd = pow(10.0, 10.0);       // Start with something big
+    double curr_max_nd = 1.0E+10;       // Start with something big
 
-    double eps = pow(10.0, -8.0);
+    double eps = 1.0E-10;
 
     int curr_iter = 0;
     int max_iter = N*N*N;
 
-    while ((curr_max_nd > eps) || ((double)curr_iter < (double)max_iter)){
+    while ((curr_max_nd > eps) && ((double)curr_iter < (double)max_iter)){
         curr_max_nd = get_max_non_diag(A, N, &k ,&l);
         get_trig_values(A, k, l, &cosine, &sine);
         ortho_transform(&A, N, k, l, cosine, sine);
         curr_iter++;
     }
 
-    std::cout << "Number of iterations done: " << curr_iter << std::endl;
-    A.print("D = ");
+    std::cout << "\nNumber of iterations done: " << curr_iter << std::endl;
+    A.print("\nD = ");
 }
