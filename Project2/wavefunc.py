d223aec8a6c1ab416a5bf177a06781eb5b93c534
tbhjelmeland <31311855+tbhjelmeland@users.noreply.github.com>
2017-09-28 14:29:05 +0200

    Add files via upload
    Plot as function of r not rho

--- Project2/wavefunc.py
+++ Project2/wavefunc.py
@@ -2,15 +2,18 @@ import numpy as np
 import matplotlib.pyplot as mpl
 #Stuff that need to be manually changed for each plot
 n=500
-omega=1.0
+omega=0.01
 
 m_e=9.10938356e-31	#kg
-hbar=1.0545718e-34	#Js
+#hbar=1.0545718e-34	#Js = 
+hbar=6.582119514e-16 	#eV s
 
+#k=m_e*omega*omega
 
-k=m_e*omega*omega
-alpha4= ((hbar*hbar)/m_e*k)
-alpha = alpha4**0.25
+beta=1.44e-9	#eV m
+
+alpha= ((hbar*hbar)/m_e*beta)
+#alpha = alpha4**0.25
 
 rho = np.linspace(0, 60.0, n)
 r=rho*alpha 
@@ -41,6 +44,6 @@ mpl.plot(r, eig2, "b-*")
 mpl.plot(r, eig3, "y-*")
 mpl.legend(['$\Psi_1$', '$\Psi_2$', '$\Psi_3$'])
 mpl.title('Graphical representation of the three first wavevectors')
-mpl.xlabel('rho')
+mpl.xlabel('r [m]')
 mpl.ylabel('Probability; |$\Psi$|^2')
 mpl.show()
