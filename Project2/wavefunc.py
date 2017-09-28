a24201de1dd1f9030624628b651f2e753e0f1a1f
tbhjelmeland <31311855+tbhjelmeland@users.noreply.github.com>
2017-09-28 12:16:35 +0200

    Add files via upload
    plotting program that plots wavefunction as function of r instead of rho

--- Project2/wavefunc.py
+++ Project2/wavefunc.py
@@ -1,12 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as mpl
+#Stuff that need to be manually changed for each plot
+n=500
+omega=1.0
 
-n=200
+m_e=9.10938356e-31	#kg
+hbar=1.0545718e-34	#Js
 
-#eig1=[]
-#with list I should append
 
-eig_1=np.zeros(n)
+k=m_e*omega*omega
+alpha4= ((hbar*hbar)/m_e*k)
+alpha = alpha4**0.25
+
+rho = np.linspace(0, 60.0, n)
+r=rho*alpha 
+
+
+
+eig1=np.zeros(n)
 eig2=np.zeros(n)
 eig3=np.zeros(n)
 
@@ -14,18 +25,20 @@ eig3=np.zeros(n)
 
 with open("outputfile", "r") as results:
 	for line_num, line_string in enumerate(results):
-            if (line_num > 0):
+	    if (line_num > 0):
                 words = line_string.split()
-                eig_1[line_num-1] = float(words[0])
+                eig1[line_num-1] = float(words[0])
                 eig2[line_num-1] = float(words[1])
 		eig3[line_num-1] = float(words[2])
 
-rho = np.linspace(0, 4.0, n) 
 
-mpl.plot(rho, eig_1, "r-*")
+
+
+
+mpl.plot(r, eig1, "r-*")
 mpl.hold('on')
-mpl.plot(rho, eig2, "b-*")
-mpl.plot(rho, eig3, "y-*")
+mpl.plot(r, eig2, "b-*")
+mpl.plot(r, eig3, "y-*")
 mpl.legend(['$\Psi_1$', '$\Psi_2$', '$\Psi_3$'])
 mpl.title('Graphical representation of the three first wavevectors')
 mpl.xlabel('rho')
